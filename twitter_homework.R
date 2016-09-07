26eda87046f172880f326449129f02393e63d187
william <wlbconstan@gmail.com>
2016-09-06 21:28:57 -0700

    Added weather, clustered distance, and parsed location variables from tweets. Added fancy plots for tweet locations. Imputed missing weather data.

--- twitter_homework.R
+++ twitter_homework.R
@@ -3,12 +3,14 @@ lib <- .Library
 repos <- 'https://cran.rstudio.com'
 
 required_packages <- c("streamR", "RCurl", "ROAuth", "RJSONIO",'ggplot2','maps','grid',
-                       'data.table','httr','twitteR','wordcloud','tm','geosphere','randomForest')
+                       'data.table','httr','twitteR','wordcloud','tm','geosphere','randomForest','stats')
 installed_packages <- .packages(all.available = TRUE, lib.loc = lib)
 missing_packages <- setdiff(required_packages, installed_packages)
 if (length(missing_packages) > 0L) {
   install.packages(pkgs = missing_packages, lib = lib, repos = repos)
 }
+
+# http://www.ncdc.noaa.gov/data-access/quick-links
 
 invisible(sapply(required_packages, function(pkg) stopifnot(require(pkg, character.only = TRUE))))
 
@@ -51,7 +53,10 @@ get_bounds <- function(database = 'county', state = 'washington', regions = NULL
 get_geo_tagged_data <- function(data) {
   stopifnot(is.data.table(data))
   stopifnot(all(c('latitude','longitude') %in% names(data)))
-  return(data[!is.na(latitude) & !is.na(longitude)])
+  data <- data[!is.na(latitude) & !is.na(longitude)]
+  data[, latitude := as.numeric(latitude)]
+  data[, longitude := as.numeric(longitude)]
+  return(data)
 }
 
 # WA/King and WA/Pierce county bounding box (approx)
@@ -90,16 +95,21 @@ access_token <- '770382304636182528-aHXQBlH4wi439uOv3FFWEbhYN1RCJwq'
 access_secret <-'RXKXr9UCyrA6tpzEPq6LJorujMS9Vnp8lzeiuR8regIxB'
 
 # registerTwitterOAuth(credentials)
-setup_twitter_oauth(consumer_key = consumer_key, consumer_secret = consumer_secret, access_token = access_token, access_secret = access_secret)
+setup_twitter_oauth(consumer_key = consumer_key, consumer_secret = consumer_secret, 
+                    access_token = access_token, access_secret = access_secret)
 
 tweet_history_user = userTimeline('ArianeAstraea', n = 3200)
 tweet_history_user <- data.table(twListToDF(tweet_history_user))
+save(tweet_history_user, file = 'tweet_history.RData')
+
+#### OBTAIN DATA FOR TRAINING
 DT <- get_geo_tagged_data(tweet_history_user)
+
 
 # Feature extraction
 DT[, day := factor(weekdays(created))]
 DT <- DT[order(created)]
-day_rank <- DT[, .N, by = day][order(N, decreasing = TRUE)] # what day is she tweeting the most?
+day_rank <- DT[, .N, by = day][order(N, decreasing = TRUE)] # what day is the user tweeting the most?
 day_rank[, day_rank := seq.int(.N)]
 
 DT <- merge(DT, day_rank, by = 'day')
@@ -117,17 +127,80 @@ daytime_category <-function(date) {
 
 DT[, day_time := factor(daytime_category(created))]
 
+# define center to be the median tweet location on lat-long coordinates
+DT[, plot(latitude, longitude, pch = 19)] # obviously 2 clusters
+# center <- DT[, c(median(latitude, na.rm = TRUE), median(longitude, na.rm = TRUE))]
+cluster <- kmeans(DT[, list(latitude, longitude)], centers = 2)
+regions <- c('PACNW', 'PHILIPPINES')
+if (cluster$centers[1L,1L] < 20) regions <- rev(regions) 
+centers <- data.table(center_latitude = cluster$centers[,1L], 
+                      center_longitude = cluster$centers[,2L], 
+                      cluster_color = c('red','blue'),
+                      region = regions,
+                      key = 'region')
+print(centers)
+DT[, region := ifelse(cluster$cluster == 1L, regions[1L], regions[2L])]
+setkey(DT, region)
+DT <- DT[centers]
+
+plot_cluster <- function(data) {
+  stopifnot(is.data.table(data))
+  stopifnot(data[, length(unique(region))] == 1L)
+
+  color <- data[, unique(cluster_color)]
+  region <- data[, unique(region)]
+  centers <- data[, list(latitude = unique(center_latitude), longitude = unique(center_longitude))]
+  
+  if (region == 'PHILIPPINES'){
+    regional_map <- map_data('world', regions = 'Philippines')
+    capital <- data.frame(latitude = 14.5995, longitude = 120.9842, name = 'Manila, Philippines')
+  } else {
+    regional_map <- map_data('county', region = c('washington'))
+    capital <- data.frame(latitude = 47.0379, longitude = -122.9007, name = 'Olympia, WA')
+  }
+  
+  g <- ggplot(regional_map) + 
+    geom_map(aes(map_id = region), map = regional_map, fill = "white", color = "grey20", size = 0.25) +
+    expand_limits(x = regional_map$long, y = regional_map$lat) +
+    geom_point(data = data, aes(x = longitude, y = latitude), size = 3, alpha = 2/5, color = color) +
+    geom_point(data = centers, 
+               aes(x = longitude, y = latitude), size = 2, stroke = 2, alpha = 1, color = 'black', fill = 'white', shape = 21) +
+    geom_point(data = capital, aes(x = longitude, y = latitude), size = 3, alpha = 1, 
+               shape = 24, color = 'black', fill = 'black') +
+    geom_point(data = capital, aes(x = longitude, y = latitude), size = 3, alpha = 1, 
+               shape = 25, color = 'black', fill = 'black') +
+    geom_text(data = capital, aes(x = longitude, y = latitude, label = name, hjust = -0.1)) +
+    xlab('longitude') + ylab('latitude')
+  
+  print(g)
+  
+  return(invisible(g))
+}
+
+# fancy plots of tweets about cluster centers
+plot_cluster(DT[region == 'PACNW'])
+plot_cluster(DT[region == 'PHILIPPINES'])
+
+vincenty_distance <- function(p1, p2, r = 3963.190592) {
+  # Vincenty elipsoid great circle distance between two points in miles
+  miles_per_meter <- 0.000621371
+  distVincentyEllipsoid(p1, p2) * miles_per_meter
+}
+
+DT[, dist_from_center := vincenty_distance(cbind(center_longitude, center_latitude), 
+                                             cbind(longitude, latitude)), 
+   by = region]
+
 # add temp data
-mean_seattle_temps <- as.matrix(read.delim('noaa_seattle_daily_temp_history.txt', 
-                                 row.names = 1L,stringsAsFactors = FALSE, 
-                                 header = FALSE, 
-                                 sep = '', 
-                                 col.names = c('Day', month.abb)))
+# http://www.ncdc.noaa.gov/cdo-web
 
-day_month_index <- DT[, cbind(as.integer(format(created, '%d')), as.integer(format(created, '%m')))]
-DT[, mean_temp := mean_seattle_temps[day_month_index]]
-DT[, plot(created, mean_temp, xlab = 'Tweet Date', ylab = 'Mean Temp (F)')]
+weather <- data.table(read.csv('weather.csv'))
+weather <- weather[grepl('Seattle|Manila', STATION_NAME, ignore.case = TRUE) & TAVG > 0]
+weather[, region := ifelse(grepl('Seattle', STATION_NAME, ignore.case = TRUE), 'PACNW', 'PHILIPPINES')]
+weather <- weather[, list(region, DATE, PRCP, TAVG, TMAX, TMIN)]
 
+DT[, DATE := as.integer(format(created, '%Y%m%d'))]
+DT <- merge(DT, weather, by = c('region', 'DATE'), all.x = TRUE)
 
 # add season
 date_to_season <- function(DATES) {
@@ -147,42 +220,119 @@ date_to_season <- function(DATES) {
 DT[, season := factor(date_to_season(created))]
 
 # form lat-long target category
-DT[, lat_long := factor(paste(round(as.numeric(latitude), 2L), round(as.numeric(longitude), 2L), sep = ':'))]
+DT[, lat_long := factor(paste(round(latitude, 2L), round(longitude, 2L), sep = ':'))]
 
 # search_string <- "#mls"
 # num_tweets <- 100
 # tweets <- searchTwitter(search_string, n=num_tweets, lang="en")
 
 # wordcloud
-words <- DT[, clean_words(text)]
-word_table <- table(words)
-wordcloud(words = names(word_table), freq = word_table, min.freq = 3L)
+# words <- DT[, clean_words(text)]
+# word_table <- table(words)
+# wordcloud(words = names(word_table), freq = word_table, min.freq = 3L)
 
-# define center to be the median tweet location on lat-long coordinates
-center <- DT[, c(median(as.numeric(latitude), na.rm = TRUE), median(as.numeric(longitude), na.rm = TRUE))]
-
-haverstine_distance <- function(p1, p2, r = 3963.190592) {
-  # Vicenty elipsoid great circle distance between two points in miles
-  miles_per_meter <- 0.000621371
-  distVincentyEllipsoid(p1, p2) * miles_per_meter
+# Tweets seem to have a lot of location identifiers like:
+#   "I'm at Bank Of America in Seattle, WA https://t.co/HhkT9UDBx0"
+# The bulk of these have an '@' or an 'at' word followed by a location
+# where each word of the location is capiltalized. 
+first_letter_is_capitalized <- function(word){
+  word_letters <- strsplit(word, '')[[1L]]
+  first_letter <- word_letters[1L]
+  is_capitalized <- first_letter == toupper(first_letter)
+  return(is_capitalized)
 }
 
-DT[, miles_from_center := haverstine_distance(rev(center), cbind(as.numeric(longitude), as.numeric(latitude)))]
+example <- DT[207, text]
+print(example)
+
+parse_tweet_location <- function(tweet) {
+  
+  remove_url <- function(x) gsub("http[^[:space:]]*", "", x)
+  
+  tweet <- remove_url(tweet)
+  tweet <- gsub('^.*\\bat\\b', 'at', tweet)
+  tweet <- gsub('[@]', ' at ', tweet)
+  tweet <- gsub('(at\\s+at)+','at', tweet)
+  tweet <- gsub('[[:punct:]]', '', tweet)
+  tweet <- gsub('[[:cntrl:]]', '', tweet)
+  tweet <- gsub("[^[:alnum:]///' ]", "", tweet)
+  
+  pattern <- '\\bat\\b'
+  if (!grepl(pattern = pattern, x = tweet)) return('')
+  
+  # split with 'at'
+  after_at <- strsplit(x = tweet, split = pattern)[[1L]][2L]
+  
+  # split into words
+  words <- strsplit(after_at, split = '\\s+')[[1L]]
+  words <- words[nzchar(words)]
+  
+  # find the words with capital letters
+  cap_words <- sapply(words, first_letter_is_capitalized)
+  
+  # Find the range of words that have their first letter capitalized
+  first_word <- which(cap_words)[1L]
+  if (is.na(first_word)) first_word <- 1L
+  last_word <- max(which(!cap_words)[1L] - 1L, 1L)
+  which_in <- grep('in', words)
+  if (length(which_in) > 0L){
+    diff_in <- which_in - last_word
+    good <- diff_in[diff_in %between% c(1,5)]
+    if (length(good) > 0L){
+      last_word <- last_word + good[1L] - 1L
+    }
+  }
+
+  if (is.na(last_word)) last_word <- length(words)
+  
+  # return the location, lower cased and collapsed
+  tolower(paste(words[first_word:last_word], collapse = '_'))
+} 
+
+example
+parse_tweet_location(example)
+
+parsed_locations <- data.table(loc = sapply(DT[, text], parse_tweet_location))
+
+# cannot have more than 53 categories or else randomForest dies
+# take the most popular
+most_popular <- parsed_locations[, .N, by=loc][order(-N)][1:53, loc]
+parsed_locations[!loc %in% most_popular, loc := '']
+parsed_locations[!nzchar(loc), loc := 'missing']
+DT[, parsed_location := factor(parsed_locations$loc)]
 
 ######
 # fit model
-rf_model <- randomForest(lat_long ~ day + day_time + mean_temp + miles_from_center + season, 
-                         data=DT, importance=TRUE, proximity=TRUE, mtry = 5)
+DT[, region := factor(region)]
+form <- formula(lat_long ~ DATE + day + day_time + TAVG + TMIN + TMAX + dist_from_center + PRCP + season + region + parsed_location)
+
+# check for missing independent variables
+ind_vars <- attr(terms(form), 'term.labels')
+rows_with_missing_data <- which(apply(DT[, ind_vars, with = FALSE], MARGIN = 1, function(x) any(is.na(x))))
+any_missing <- length(rows_with_missing_data) > 0L
+if (any_missing) {
+  cat('\nRows with missing data:\n');flush.console()
+  DT[rows_with_missing_data, ind_vars, with = FALSE]
+}
+
+DT <- data.table(rfImpute(form, data = DT))
+
+if (any_missing) {
+  cat('\nRows that HAD missing data:\n');flush.console()
+  DT[rows_with_missing_data, ind_vars, with = FALSE]
+}
+
+rf_model <- randomForest(form, data=DT, importance=TRUE, proximity=TRUE, mtry = 5)
 
 # variable importance
 varImpPlot(rf_model)
 
 rf_model_predict <- function(rf_model) {
-
+  
   pred_mat <- predict(rf_model, type='prob')
   classes <- data.table(do.call(rbind.data.frame, strsplit(colnames(pred_mat), split = ':')))
   setnames(classes, c('lat','lon'))
-
+  
   best_prob <- function(probs){
     ix <- which.max(probs)
     cbind(classes[ix], 
